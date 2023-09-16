070d097976ff530aaa5b30a9cd9154599c2411de
nana-khay <nanakhay123456789@gmail.com>
2023-09-15 20:03:21 -0700

    A function that produces output according to a format.

---
+++ _printf.c
@@ -0,0 +1,60 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int count = 0;
+    char c;
+    char *str;
+
+    va_start(args, format);
+
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++; 
+
+            switch (*format)
+            {
+                case 'c':
+                    c = va_arg(args, int);
+                    putchar(c);
+                    count++;
+                    break;
+                case 's':
+                    str = va_arg(args, char *);
+                    if (str == NULL)
+                        str = "(null)";
+                    while (*str)
+                    {
+                        putchar(*str);
+                        str++;
+                        count++;
+                    }
+                    break;
+                case '%':
+                    putchar('%');
+                    count++;
+                    break;
+                default:
+                    putchar('%');
+                    putchar(*format);
+                    count += 2;
+                    break;
+            }
+        }
+        else
+        {
+            putchar(*format);
+            count++;
+        }
+        format++;
+    }
+
+    va_end(args);
+    return count;
+}
+

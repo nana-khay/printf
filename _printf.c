0599eacc180a492e51445a1d6eab7650b46705e1
osman-gif <osmanngong@gmail.com>
2023-09-16 15:09:04 +0200

    added _putchar.c decide.c lists.h files and modified the other files

--- _printf.c
+++ _printf.c
@@ -1,14 +1,14 @@
-#include "main.h"
+/*#include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
-
+*/
 /**
  * _printf - Printf function
  * @format: format
  * Return: conversions
  */
 
- int _printf(const char *format, ...)
+/* int _printf(const char *format, ...)
 {
     va_list args;
     int count = 0;
@@ -70,4 +70,60 @@
     va_end(args);
     return count;
 }
+*/
+#include "lists.h"
+/**
+ * _printf - Custom printf function implementation
+ * @format: The format string containing placeholders
+ * @...: Additional arguments for placeholders
+ *
+ * Description: This function simulates the behavior of the printf function
+ *
+ * Return: The total count of characters printed.
+ */
 
+
+int _printf(const char *format, ...)
+{
+        int cnt = 0;
+        int i = 0;
+        va_list args;
+
+        va_start(args, format);
+
+        while (format[i] != '\0')
+        {
+                cnt++;
+                if (format[i] == '%')
+                {
+                        if (format[i + 1] == '%')
+                        {
+
+                        }
+                        if (format[i + 1] != '\0')
+                        {
+                                int (*function)(va_list) = decision(format[i + 1]);
+
+                                if (function != NULL)
+                                {
+                                        /*cnt +=*/
+                                        function(args);
+                                        i++;
+                                }
+                                else
+                                {
+                                        _putchar(format[i]);
+                                        /*cnt++;*/
+                                }
+                        }
+                }
+                else
+                {
+                        _putchar(format[i]);            }
+                i++;
+        }
+
+        va_end(args);
+        return (cnt);
+}
+~

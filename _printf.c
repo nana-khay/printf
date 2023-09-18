d4109f1231cbf25afb7aab70ef5a2f4bd0fc1d3c
maundu55 <marymaundu.mm@gmail.com>
2023-09-18 12:18:53 -0700

    updates

--- _printf.c
+++ _printf.c
@@ -17,38 +17,31 @@ int _printf(const char * const format, ...)
 		{"%o", print_octal}, {"%x", print_hex}, {"%X", print_HEX},
 		{"%S", print_special_string}, {"%p", print_pointer}
 	};
-
-	va_list args; 
+	va_list args;
 	int i = 0, j, length = 0;
 
-	va_start(args, format); 
+	va_start(args, format);
 
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-
 	while (format[i] != '\0')
 	{
 		j = 13;
 		while (j >= 0)
 		{
-
 			if (format_specifiers[j].id[0] == format[i] && format_specifiers[j].id[1] == format[i + 1])
-			{
-				length += format_specifiers[j].f(args); 
-				i = i + 2; 
-				goto Continue; 
+			{length += format_specifiers[j].f(args);
+				i = i + 2;
+				goto Continue;
 			}
 			j--;
 		}
-
 		_putchar(format[i]);
 		length++;
 		i++;
 Continue:
 		continue;
 	}
-
-	va_end(args); 
+	va_end(args);
 	return (length);
-
 }

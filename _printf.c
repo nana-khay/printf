eeb9c3333db7767443ea4d1f5ba2f4ecd4c93cfe
nana-khay <nanakhay123456789@gmail.com>
2023-09-18 03:52:45 -0700

    Update

--- _printf.c
+++ _printf.c
@@ -1,10 +1,10 @@
-#include "main.h"
+/**#include "main.h"
 
-/**
+
  * _printf - A function for formatted printing, determining the correct format.
  * @format: The format string to process.
  * Return: The length of the printed output.
- */
+ 
 int _printf(const char * const format, ...)
 {
    
@@ -49,4 +49,60 @@ int _printf(const char * const format, ...)
 
     va_end(args); 
     return (length);
+}
+*/
+
+
+
+
+
+#include "main.h"
+
+/**
+ * _printf - prints formatted data to stdout
+ * @format: string that contains the format to print
+ * Return: number of characters written
+ */
+int _printf(char *format, ...)
+{
+	int written = 0, (*structype)(char *, va_list);
+	char q[3];
+	va_list pa;
+
+	if (format == NULL)
+		return (-1);
+	q[2] = '\0';
+	va_start(pa, format);
+	_putchar(-1);
+	while (format[0])
+	{
+		if (format[0] == '%')
+		{
+			structype = driver(format);
+			if (structype)
+			{
+				q[0] = '%';
+				q[1] = format[1];
+				written += structype(q, pa);
+			}
+			else if (format[1] != '\0')
+			{
+				written += _putchar('%');
+				written += _putchar(format[1]);
+			}
+			else
+			{
+				written += _putchar('%');
+				break;
+			}
+			format += 2;
+		}
+		else
+		{
+			written += _putchar(format[0]);
+			format++;
+		}
+	}
+	_putchar(-2);
+	return (written);
 }
--- main.h
+++ main.h
@@ -1,4 +1,4 @@
-#ifndef MAIN_H
+/**#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
@@ -7,11 +7,11 @@
 #include <limits.h>
 #include <unistd.h>
 
-/**
+
  * struct convert_match - match the conversion specifiers for custom_printf
  * @id: a string representing the specifier, e.g., "%d", "%s"
  * @f: a function pointer for the conversion specifier
- */
+ 
 typedef struct convert_match
 {
     char *id;
@@ -40,4 +40,45 @@ int print_string(va_list val);
 int _putchar(char ch);
 int _printf(const char *format, ...);
 
+#endif
+*/
+
+
+
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * struct structprint - structure containing
+ * @q: the location and method to translate data to characters.
+ * @u: print function for specific type.
+ *
+ * Return: int
+ */
+typedef struct structprint
+{
+	char *q;
+	int (*u)(char *format, va_list);
+} structype;
+
+int _putchar(char ch);
+int _puts(char *string);
+int printc(char *format, va_list);
+int printstr(char *format, va_list);
+int (*driver(char *format))(char *format, va_list);
+int _printf(char *format, ...);
+int printint(char *format, va_list pa);
+int integer(int number);
+int contadordigit(int number);
+int _abs(int number);
+int printpercent(char *format, va_list pa);
+int printhex(char *format, va_list);
+int printHEX(char *format, va_list);
+int printocta(char *format, va_list);
+int print_unsign(char *format, va_list);
 #endif
